d8c20a419f32993da1c2feeb52393cc0644cc520
Ahmed Elmasry <51840899+ahmedElmasry96@users.noreply.github.com>
2025-01-30 12:13:04 +0200

    Update BaseRepositoryInterface.php

--- app/Repositories/Api/Contracts/BaseRepositoryInterface.php
+++ app/Repositories/Api/Contracts/BaseRepositoryInterface.php
@@ -1,12 +1,21 @@
 <?php
 
-namespace App\Repositories\Api\Contracts;
+namespace App\Repositories\Contracts;
+
+use App\Http\Resources\BaseResource;
+use App\Http\Resources\PaginatedResource;
 
 interface BaseRepositoryInterface
 {
-    public function all();
-    public function find($id);
-    public function create(array $data);
-    public function update($id, array $data);
-    public function delete($id);
+    public function all(
+        string $orderBy = 'DESC',
+        array $with = [],
+        array $filters = [],
+        array $searchColumns = [],
+    ): BaseResource|PaginatedResource;
+
+    public function find($id, array $with = []): BaseResource;
+    public function create(array $data): BaseResource;
+    public function update($id, array $data): BaseResource;
+    public function delete($id): bool;
 }
